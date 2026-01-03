f6a8fe8371351e8a8e77ff033bc7b3dfb7de50e7
vincecx <104307030+vincecx@users.noreply.github.com>
2026-01-03 16:27:23 +0000

    Deploying to gh-pages from @ vincecx/vincecx.github.io@5ebc4aa8d6e76cca558d5c8a53dc1848634f3c46 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-posts",
+          title: "posts",
           description: "",
           section: "Navigation",
           handler: () => {
