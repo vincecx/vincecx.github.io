b4dcacadb90a3c899c832bbccaa9e5503fd0753d
vincecx <104307030+vincecx@users.noreply.github.com>
2026-01-03 18:16:35 +0000

    Deploying to gh-pages from @ vincecx/vincecx.github.io@a32c23d611862d89bae6e5e04657455dc29a9b6c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-e-handout-for-aps-global-physics-summit-2026",
+        
+          title: "E-Handout for APS Global Physics Summit 2026",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/APS-Summit/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
